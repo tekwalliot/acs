de080217bc9c4d39aa9fd73295df5dc30b6dbe79
psbaswserver1 <psb.awsserver1@gmail.com>
2021-03-25 23:50:35 +0530

    Initial Commit

--- acs/urls.py
+++ acs/urls.py
@@ -15,9 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls, name="admin"),
     path('', include('rms.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path(
+        'change-password/',
+        auth_views.PasswordChangeView.as_view(
+            template_name='change-password.html',
+            success_url = '/'
+        ),
+        name='change_password'
+    ),
 ]
